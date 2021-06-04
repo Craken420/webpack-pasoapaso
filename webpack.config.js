99e2bd3a26273745b2ff5dc89711aa87be718c6b
Luis Angel Peña <lapena@mavi.mx>
2021-06-04 13:32:42 -0500

    feat:: Transpile the css files with postcss using next

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,11 @@ module.exports = {
             {
                 test: /\.css$/,
                 exclude: /node_modules/,
-                use: ['style-loader', 'css-loader']
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'postcss-loader'
+                ]
             }
         ]
     },
