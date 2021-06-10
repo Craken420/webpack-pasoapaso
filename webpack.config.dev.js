6cf5628865a9788ab5903842a6afbac8c9087ae6
Luis Peña <pezu.luis01@gmail.com>
2021-06-10 14:06:20 -0500

    feat:: Use the webpack server

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,7 +2,12 @@ const webpack = require('webpack');
 
 module.exports = {
     mode: 'development',
-    devtool: 'inline-source-map', // Depurar elcódigo compilado
+    devtool: 'inline-source-map', // Depurar elcódigo compilado,
+    devServer: {
+        port: 9001,
+        contentBase: path.join(__dirname, 'assets'),
+        headers: { "Access-Control-Allow-Origin": "*" }
+    },
     plugins: [
         new webpack.DefinePlugin({
             PRODUCTION: false
